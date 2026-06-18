5b7aa96700b22143e2296e238670f11d823b5690
Nex1sG <gleblatyshev1327@gmail.com>
2026-06-18 23:23:04 +0300

    refactor engine work

--- Тестовые команды/engine_launch_and_stop.py
+++ Тестовые команды/engine_launch_and_stop.py
@@ -2,25 +2,36 @@ from pioneer_sdk2 import Pioneer, Event
 from time import sleep
 
 '''
-Эта часть кода должна запускаться на Radxa,
- так как подписка на события работает только при локальном подключении к беспилотнику.
+Чтобы исполнить код, нужно:
+1) Вставить АКБ и дождаться wifi с именем  «RaZero-…..»
+2) Подключиться к wifi с паролем geoscan123
+3) Для доступа к функционалу модуля, введите в строку браузера IP-адрес и порт:
+10.42.0.1:(9090 - главное меню, 2020 - Блочное программирование,
+9999 - программирование в вс коде, 7777 - загрузка модели ИИ)
+
+В Случае обычного запуска через локалку порт будет 20556
+4) Подключение происходит по tcp="10.42.0.1:20556"
 '''
 
-test_function = lambda : print("Сработала подписка на события")
+def test_of_subscribe_function(*args):
+    print("ENGINE_STARTED")
+    print(args)
 
 pioneer = Pioneer(tcp="10.42.0.1:20556", wait_callback=True, safety_command=True)
-pioneer.subscribe(Event.ENGINE_STARTED, test_function)
+pioneer.subscribe(Event.ENGINE_STARTED, test_of_subscribe_function)
 
 try:
     print("Попытка запустить двигатели...")
     if not pioneer.arm():
         raise RuntimeError("Не удалось запустить двигатели")
+
+    sleep(3)
     print("Двигатели успешно запущены!")
 
-except RuntimeError:
-    print("Не удалось запустить двигатели")
+except Exception as e:
+    print(f"Ошибка: {e}")
 
 finally:
     pioneer.disarm()
-    pioneer.close_connection
+    pioneer.close_connection()
     print("Двигатели остановлены и соединение закрыто.")
